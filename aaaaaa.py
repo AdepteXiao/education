8cab50ea2700113862e4a8394d692b27faf73229
adeptexiao <ademkhina@list.ru>
2021-09-14 14:06:24 +0700

    1st commit

---
+++ aaaaaa.py
@@ -0,0 +1,55 @@
+"""
+КИ21 - 17/1b Демёхина Арина Руслановна
+ОП Лабораторная №1
+"""
+
+
+def just_run_it_already():
+    """
+    Функция - ввод и проверка чисел на факт существования в множестве чисел нивена
+    :return:
+    """
+    z = 0
+    number = 0
+    while z == 0:
+        number = input('Введите положительное натуральное число для проверки:')
+        if not number.isdigit():
+            print('Ошибка, некорректные входные данные')
+        else:
+            number = int(number)
+            z = 1
+    if number == 0:
+        print('Число не является числом нивена')
+        print('Число не является множественным числом нивена')
+    else:
+        first = number
+        first_sum_num = 0
+        second_sum_num = 0
+        while first > 0:
+            first_sum_num += first % 10
+            first //= 10
+        if number % first_sum_num == 0:
+            print('Число является числом нивена')
+            second = number // first_sum_num
+            while second > 0:
+                second_sum_num += second % 10
+                second //= 10
+            if (number // first_sum_num) % second_sum_num == 0:
+                print('Число является множественным числом нивена')
+            else:
+                print('Число не является множественным числом нивена')
+        else:
+            print('Число не является числом нивена')
+            print('Число не является множественным числом нивена')
+
+
+print('Числа Нивена — натуральные числа, делящиеся нацело на сумму своих цифр.')
+text = 'meh'
+while text != 'нет':
+    print('Введите "да", чтобы проверить число, или "нет", чтобы завершить программу')
+    if (text := input()) == 'да':
+        just_run_it_already()
+    elif text == 'нет':
+        print('Программа завершена')
+    else:
+        print('Ошибка')
