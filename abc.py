10df10520122cc53703f0a27ad0e2607e1495e3d
adeptexiao <ademkhina@list.ru>
2021-09-21 23:49:46 +0700

    1_commit

--- abc.py
+++ abc.py
@@ -5,6 +5,6 @@ alphabet_upper = list('a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, 
 alphabet_lower = list('A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z')
 while True:
     for element in range(1, len(z) + 1):
-        if element in alphabet_upper:
+        if z[element] in alphabet_upper:
             in_dex = alphabet_upper.index()
             print(element)
